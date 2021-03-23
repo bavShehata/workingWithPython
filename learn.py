62b3315e5f1836c98bcfba74ecf90988ded92a0f
bavshehata <bavshehata@gmail.com>
2021-03-23 14:10:17 +0200

    Zip and Enumerate

--- learn.py
+++ learn.py
@@ -117,5 +117,58 @@ while odd_count <= 5 and i < len(num_list):
     if num_list[i] % 2:
         sum += num_list[i]
     i+=1
-    
+
 print(sum)
+
+
+#zip and enumerate
+
+#label: x, y, z
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+# write your for loop here
+for l,x,y,z in zip(labels,x_coord,y_coord,z_coord):
+    points.append("{}: {}, {}, {}".format(l,x,y,z))
+
+for point in points:
+    print(point)
+
+
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
+
+
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names,heights=zip(*cast)
+
+print(names)
+print(heights)
+
+
+
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+
+print(data_transpose)
+
+
+
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+# write your for loop here
+for i,name in enumerate(cast):
+    cast[i] += " " + str(heights[i])
+
+print(cast)
