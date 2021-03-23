daddb8fab80e42d049136223930de543eaa466f7
bavshehata <bavshehata@gmail.com>
2021-03-23 13:41:08 +0200

    Loops

--- learn.py
+++ learn.py
@@ -29,3 +29,93 @@ else:
     result = "Oh dear, no prize this time."
 
 print(result)
+
+
+#loops
+cities = ["luxor", "aswan", "assuit"]
+for city in cities:
+    print(city)
+
+
+
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+for i in range(len(names)):
+    names[i] = names[i].lower().replace(" ", "_")
+print(names)
+
+
+items = ['first string', 'second string']
+html_str = "<ul>\n"  # "\ n" is the character that marks the end of the line, it does
+                     # the characters that are after it in html_str are on the next line
+
+# write your code here
+for item in items:
+    html_str += "<li>" + item + "</li>\n"
+html_str+="</ul>\n"
+print(html_str)
+
+
+
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits, but you do not want to count the other items in your basket.
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+
+for key,value in basket_items.items():
+    if key in fruits:
+        result += value
+#if the key is in the list of fruits, add the value (number of fruits) to result
+
+
+print(result)
+
+
+
+
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# write your while loop here
+
+while number > 0:
+    product *= number
+    number -= 1
+
+# print the factorial of number
+print(product)
+
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# write your for loop here
+for i in range(2, number + 1): 
+    product *= i
+
+# print the factorial of number
+print(product)
+
+
+num_list = [422, 136, 524, 85, 96, 719, 85, 92, 10, 17, 312, 542, 87, 23, 86, 191, 116, 35, 173, 45, 149, 59, 84, 69, 113, 166]
+
+odd_count = 0
+sum = 0
+i = 0
+while odd_count <= 5 and i < len(num_list):
+    if num_list[i] % 2:
+        sum += num_list[i]
+    i+=1
+    
+print(sum)
