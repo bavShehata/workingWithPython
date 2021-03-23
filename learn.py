4c4dc5105ad6c02c527fd32ca2f085b9089a5cdc
bavshehata <bavshehata@gmail.com>
2021-03-23 14:52:55 +0200

    list comprehensives

--- learn.py
+++ learn.py
@@ -172,3 +172,28 @@ for i,name in enumerate(cast):
     cast[i] += " " + str(heights[i])
 
 print(cast)
+
+
+
+# list comprehensives
+
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+
+
+multiples_3 = [num*3 for num in range(20)]
+print(multiples_3)
+
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name in scores if scores[name] >= 65 ]
+print(passed)
